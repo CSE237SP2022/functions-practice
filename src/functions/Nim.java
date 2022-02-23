c478d83a6c1c35c072985924fd899e7d426c3985
Doug Shook <dshook@wustl.edu>
2022-02-23 12:55:42 -0600

    Nim with methods

--- src/functions/Nim.java
+++ src/functions/Nim.java
@@ -6,38 +6,70 @@ public class Nim {
 	
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		int numberOfSticks = -1;
-		while(numberOfSticks < 0) {
-			numberOfSticks = ap.nextInt("How many sticks would you like to start with?");
-		}
+		int numberOfSticks = getPositiveInt(ap);
 		boolean humansTurn = true;
 		int round = 0;
 		
 		while(numberOfSticks > 0) {
 			int sticksTaken = -1;
-			System.out.print("Round " + round + ": " + numberOfSticks + " at start ");
 			if(humansTurn) {
-				while (sticksTaken < 0 || sticksTaken > numberOfSticks || sticksTaken > 2) {
-					sticksTaken = ap.nextInt("How many sticks would you like to take?");
-				}
-				System.out.print("human took " + sticksTaken);
-
+				sticksTaken = getHumanMove(ap, numberOfSticks, sticksTaken);
 			} else {
-				while (sticksTaken < 0 || sticksTaken > numberOfSticks) {
-					sticksTaken = (int)(Math.random() * 2 + 1);
-				}
-				System.out.print("computer took " + sticksTaken);
+				sticksTaken = getComputerMove(numberOfSticks, sticksTaken);
 			}
-			System.out.println(", so " + (numberOfSticks - sticksTaken) + " sticks remain.");
+			printRound(round, numberOfSticks, sticksTaken, humansTurn);
+			
 			humansTurn = !humansTurn;
 			numberOfSticks -= sticksTaken;
 			round++;
 		}
+		printWinner(humansTurn);
+	}
+
+	private static void printWinner(boolean humansTurn) {
 		if(humansTurn) {
 			System.out.println("The computer wins!");
 		} else {
 			System.out.println("You win!");
-
 		}
+	}
+	
+	private static void printRound(int round, int numberOfSticks, int sticksTaken, boolean humansTurn) {
+		System.out.print("Round " + round + ": " + numberOfSticks + " at start ");
+		
+		if(humansTurn) {
+			System.out.print("human took " + sticksTaken);
+		} else {
+			System.out.print("computer took " + sticksTaken);
+		}
+
+		System.out.println(", so " + (numberOfSticks - sticksTaken) + " sticks remain.");
+
+	}
+
+	private static int getComputerMove(int numberOfSticks, int sticksTaken) {
+		while (isInvalidMove(numberOfSticks, sticksTaken)) {
+			sticksTaken = (int)(Math.random() * 2 + 1);
+		}
+		return sticksTaken;
+	}
+
+	private static boolean isInvalidMove(int numberOfSticks, int sticksTaken) {
+		return sticksTaken < 0 || sticksTaken > numberOfSticks || sticksTaken > 2;
+	}
+
+	private static int getHumanMove(ArgsProcessor ap, int numberOfSticks, int sticksTaken) {
+		while (isInvalidMove(numberOfSticks, sticksTaken)) {
+			sticksTaken = ap.nextInt("How many sticks would you like to take?");
+		}
+		return sticksTaken;
+	}
+
+	private static int getPositiveInt(ArgsProcessor ap) {
+		int numberOfSticks = -1;
+		while(numberOfSticks < 0) {
+			numberOfSticks = ap.nextInt("How many sticks would you like to start with?");
+		}
+		return numberOfSticks;
 	}
 }
